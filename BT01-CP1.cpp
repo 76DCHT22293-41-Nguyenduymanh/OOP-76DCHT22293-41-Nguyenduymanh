20ff77b47b21e50fed040e0ef0bc248ad9897f83
76DCHT22293-41-Nguyenduymanh <duymanh3102007@gmail.com>
2026-08-18 16:01:02 +0700

    Create BT01-CP1.cpp
    BT01-FINAL; Hoan thanh bai tap

---
+++ BT01-CP1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct Nhanvien{
+	string maNV;
+	string tenNV;
+	double luongCoBan;
+	float heSo; 
+}; 
+int main(){
+	int n;
+	Nhanvien dsNV[100];
+	
+	 return 0; 
+} 
